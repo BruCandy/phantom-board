4c814fd766a3f2754129341a21b10d2b7b743ded
brucandy-lab <brucandy-lab@users.noreply.github.com>
2026-04-14 10:39:09 +0900

    fix: correct the icon path

--- resources/ui_setup.sh
+++ resources/ui_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.local/share/icons/hicolor/48x48/apps/
-cp ../ui/data/icon_gray.png ~/.local/share/icons/hicolor/48x48/apps/
-cp ../ui/data/icon_white.png ~/.local/share/icons/hicolor/48x48/apps/
+cp ../ui/data/icons/icon_gray.png ~/.local/share/icons/hicolor/48x48/apps/
+cp ../ui/data/icons/icon_white.png ~/.local/share/icons/hicolor/48x48/apps/
 
 mkdir -p ~/.local/share/icons/hicolor
 cp ../ui/theme/index.theme ~/.local/share/icons/hicolor/
